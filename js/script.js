02bd254c20396207663e9b16cffd69a265fe31cb
Prajin Kamble <73735407+Prajin-Kamble@users.noreply.github.com>
2024-10-04 17:48:57 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ async function getSongs(folder) {
     currFolder = folder;
     console.log(folder)
     console.log(`/${folder}/`)
-    let a = await fetch(`/${folder}/cover.jpg`)
+    let a = await fetch(`/${folder}/`)
     console.log(a)
     let response = await a.text();
     let div = document.createElement("div")
