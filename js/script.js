acfa1db4dd02f51b24f6642c521dd7cc1b077f7c
Prajin Kamble <73735407+Prajin-Kamble@users.noreply.github.com>
2024-10-04 12:25:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ async function getSongs(folder) {
     currFolder = folder;
     console.log(folder)
     console.log(`/${folder}/`)
-    let a = await fetch(`/${folder}/`)
+    let a = await fetch(`https://github.com/Prajin-Kamble/Spotify-Clone/tree/48382e78ee73357187b229a68dc7db1c43125a5a/${folder}/`)
      console.log(a)
     let response = await a.text();
     let div = document.createElement("div")
